772adaea7946b901d6f878f593599a7758737884
notjuliet <notjuliet@riseup.net>
2024-07-21 09:19:38 +0200

    almost finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+// TODO:
+// - error for login
+//
+
 import {
   createSignal,
   For,
@@ -12,7 +16,6 @@ import styles from "./App.module.css";
 import { BskyAgent } from "@atproto/api";
 
 enum RepoStatus {
-  ACTIVE,
   BLOCKEDBY,
   DELETED,
   DEACTIVATED,
@@ -28,30 +31,7 @@ type FollowRecord = {
 };
 
 let [followRecords, setFollowRecords] = createStore<FollowRecord[]>([]);
-let [progress, setProgress] = createSignal(0);
-let [followCount, setFollowCount] = createSignal(0);
-
-const fetchFollows = async (agent: any) => {
-  const PAGE_LIMIT = 100;
-  const fetchPage = async (cursor?: any) => {
-    return await agent.com.atproto.repo.listRecords({
-      repo: agent.session.did,
-      collection: "app.bsky.graph.follow",
-      limit: PAGE_LIMIT,
-      cursor: cursor,
-    });
-  };
-
-  let res = await fetchPage();
-  let follows = res.data.records;
-
-  while (res.data.cursor && res.data.records.length >= PAGE_LIMIT) {
-    res = await fetchPage(res.data.cursor);
-    follows = follows.concat(res.data.records);
-  }
-
-  return follows;
-};
+let agent: BskyAgent;
 
 const resolveHandle = async (handle: string) => {
   const agent = new BskyAgent({
@@ -83,147 +63,95 @@ const fetchServiceEndpoint = async (handle: string) => {
   });
 };
 
-function selectRecords(status: RepoStatus) {
-  followRecords.forEach((record, index) => {
-    if (record.status == status)
-      setFollowRecords(index, "toBeDeleted", record.toBeDeleted ? false : true);
-  });
-}
-
-const fetchHiddenAccounts = async (handle: string, password: string) => {
+const loginBsky = async (handle: string, password: string) => {
   const serviceURL = await fetchServiceEndpoint(handle);
 
-  const agent = new BskyAgent({
+  agent = new BskyAgent({
     service: serviceURL,
   });
 
-  try {
-    await agent.login({
-      identifier: handle,
-      password: password,
-    });
-  } catch (e: any) {
-    return;
-  }
-
-  await fetchFollows(agent).then((follows) =>
-    follows.forEach((record: any) => {
-      setFollowRecords(followRecords.length, {
-        did: record.value.subject,
-        handle: "",
-        uri: record.uri,
-        status: RepoStatus.ACTIVE,
-        toBeDeleted: false,
-      });
-    }),
-  );
-
-  setProgress(0);
-  setFollowCount(followRecords.length);
-
-  followRecords.forEach(async (record, index) => {
-    try {
-      const res = await agent.getProfile({ actor: record.did });
-      if (res.data.viewer?.blockedBy) {
-        setFollowRecords(index, "handle", res.data.handle);
-        setFollowRecords(index, "status", RepoStatus.BLOCKEDBY);
-      }
-    } catch (e: any) {
-      const res = await fetch(
-        record.did.startsWith("did:web")
-          ? "https://" + record.did.split(":")[2] + "/.well-known/did.json"
-          : "https://plc.directory/" + record.did,
-      );
-
-      setFollowRecords(
-        index,
-        "handle",
-        await res.json().then((doc) => {
-          for (const alias of doc.alsoKnownAs) {
-            if (alias.includes("at://")) {
-              return alias.split("//")[1];
-            }
-          }
-        }),
-      );
-
-      if (e.message.includes("not found")) {
-        setFollowRecords(index, "status", RepoStatus.DELETED);
-      } else if (e.message.includes("deactivated")) {
-        setFollowRecords(index, "status", RepoStatus.DEACTIVATED);
-      } else if (e.message.includes("suspended")) {
-        setFollowRecords(index, "status", RepoStatus.SUSPENDED);
-      }
-    }
-    setProgress(progress() + 1);
+  await agent.login({
+    identifier: handle,
+    password: password,
   });
-
-  //  setFollowCount(0);
-  //
-  //  const unfollowCount = followRecords.filter(
-  //    (record) => record.toBeDeleted,
-  //  ).length;
-  //
-  //  const writes = followRecords
-  //    .filter((record) => record.toBeDeleted)
-  //    .map((record) => {
-  //      return {
-  //        $type: "com.atproto.repo.applyWrites#delete",
-  //        collection: "app.bsky.graph.follow",
-  //        rkey: record.uri.split("/").pop(),
-  //      };
-  //    });
-  //
-  //  const BATCHSIZE = 200;
-  //  if (agent.session) {
-  //    for (let i = 0; i < writes.length; i += BATCHSIZE) {
-  //      await agent.com.atproto.repo.applyWrites({
-  //        repo: agent.session.did,
-  //        writes: writes.slice(i, i + BATCHSIZE),
-  //      });
-  //    }
-  //  }
 };
 
-const Records: Component = () => {
+const Follows: Component = () => {
+  function selectRecords(status: RepoStatus) {
+    followRecords.forEach((record, index) => {
+      if (record.status == status)
+        setFollowRecords(
+          index,
+          "toBeDeleted",
+          record.toBeDeleted ? false : true,
+        );
+    });
+  }
+
   return (
     <div>
+      <Show when={followRecords.length}>
+        <div>
+          <button
+            type="button"
+            onclick={() => selectRecords(RepoStatus.DELETED)}
+          >
+            Deleted
+          </button>
+          <button
+            type="button"
+            onclick={() => selectRecords(RepoStatus.DEACTIVATED)}
+          >
+            Deactivated
+          </button>
+          <button
+            type="button"
+            onclick={() => selectRecords(RepoStatus.BLOCKEDBY)}
+          >
+            Blocked By
+          </button>
+          <button
+            type="button"
+            onclick={() => selectRecords(RepoStatus.SUSPENDED)}
+          >
+            Suspended
+          </button>
+        </div>
+      </Show>
       <For each={followRecords}>
         {(record, index) => (
-          <Show when={record.status != RepoStatus.ACTIVE}>
-            <div>
-              <input
-                type="checkbox"
-                id="delete"
-                checked={record.toBeDeleted}
-                onChange={(e) =>
-                  setFollowRecords(
-                    index(),
-                    "toBeDeleted",
-                    e.currentTarget.checked,
-                  )
-                }
-              />
-              <span>{record.handle} </span>
-              <span>
-                <Switch>
-                  <Match when={record.status == RepoStatus.DELETED}>
-                    Deleted
-                  </Match>
-                  <Match when={record.status == RepoStatus.DEACTIVATED}>
-                    Deactivated
-                  </Match>
-                  <Match when={record.status == RepoStatus.BLOCKEDBY}>
-                    Blocked by
-                  </Match>
-                  <Match when={record.status == RepoStatus.SUSPENDED}>
-                    Suspended
-                  </Match>
-                </Switch>
-              </span>
-              <span> {record.did}</span>
-            </div>
-          </Show>
+          <div>
+            <input
+              type="checkbox"
+              id="delete"
+              checked={record.toBeDeleted}
+              onChange={(e) =>
+                setFollowRecords(
+                  index(),
+                  "toBeDeleted",
+                  e.currentTarget.checked,
+                )
+              }
+            />
+            <span>{record.handle} </span>
+            <span>
+              <Switch>
+                <Match when={record.status == RepoStatus.DELETED}>
+                  Deleted
+                </Match>
+                <Match when={record.status == RepoStatus.DEACTIVATED}>
+                  Deactivated
+                </Match>
+                <Match when={record.status == RepoStatus.BLOCKEDBY}>
+                  Blocked by
+                </Match>
+                <Match when={record.status == RepoStatus.SUSPENDED}>
+                  Suspended
+                </Match>
+              </Switch>
+            </span>
+            <span> {record.did}</span>
+          </div>
         )}
       </For>
     </div>
@@ -233,6 +161,114 @@ const Records: Component = () => {
 const Form: Component = () => {
   const [handle, setHandle] = createSignal("");
   const [password, setPassword] = createSignal("");
+  const [progress, setProgress] = createSignal(0);
+  const [followCount, setFollowCount] = createSignal(0);
+
+  const fetchHiddenAccounts = async (handle: string, password: string) => {
+    const fetchFollows = async (agent: any) => {
+      const PAGE_LIMIT = 100;
+      const fetchPage = async (cursor?: any) => {
+        return await agent.com.atproto.repo.listRecords({
+          repo: agent.session.did,
+          collection: "app.bsky.graph.follow",
+          limit: PAGE_LIMIT,
+          cursor: cursor,
+        });
+      };
+
+      let res = await fetchPage();
+      let follows = res.data.records;
+
+      while (res.data.cursor && res.data.records.length >= PAGE_LIMIT) {
+        res = await fetchPage(res.data.cursor);
+        follows = follows.concat(res.data.records);
+      }
+
+      return follows;
+    };
+
+    await loginBsky(handle, password);
+    if (!agent) return;
+
+    await fetchFollows(agent).then((follows) =>
+      follows.forEach(async (record: any) => {
+        setFollowCount(follows.length);
+
+        try {
+          const res = await agent.getProfile({ actor: record.value.subject });
+          if (res.data.viewer?.blockedBy) {
+            setFollowRecords(followRecords.length, {
+              did: record.value.subject,
+              handle: res.data.handle,
+              uri: record.uri,
+              status: RepoStatus.BLOCKEDBY,
+              toBeDeleted: false,
+            });
+          }
+        } catch (e: any) {
+          const res = await fetch(
+            record.value.subject.startsWith("did:web")
+              ? "https://" +
+                  record.value.subject.split(":")[2] +
+                  "/.well-known/did.json"
+              : "https://plc.directory/" + record.value.subject,
+          );
+
+          let status;
+          if (e.message.includes("not found")) {
+            status = RepoStatus.DELETED;
+          } else if (e.message.includes("deactivated")) {
+            status = RepoStatus.DEACTIVATED;
+          } else if (e.message.includes("suspended")) {
+            status = RepoStatus.SUSPENDED;
+          }
+
+          const handle = await res.json().then((doc) => {
+            for (const alias of doc.alsoKnownAs) {
+              if (alias.includes("at://")) {
+                return alias.split("//")[1];
+              }
+            }
+          });
+
+          setFollowRecords(followRecords.length, {
+            did: record.value.subject,
+            handle: handle,
+            uri: record.uri,
+            status: status,
+            toBeDeleted: false,
+          });
+        }
+        setProgress(progress() + 1);
+      }),
+    );
+  };
+
+  const unfollow = async () => {
+    const writes = followRecords
+      .filter((record) => record.toBeDeleted)
+      .map((record) => {
+        return {
+          $type: "com.atproto.repo.applyWrites#delete",
+          collection: "app.bsky.graph.follow",
+          rkey: record.uri.split("/").pop(),
+        };
+      });
+
+    const BATCHSIZE = 200;
+    if (agent.session) {
+      for (let i = 0; i < writes.length; i += BATCHSIZE) {
+        await agent.com.atproto.repo.applyWrites({
+          repo: agent.session.did,
+          writes: writes.slice(i, i + BATCHSIZE),
+        });
+      }
+    }
+
+    setFollowRecords([]);
+    setProgress(0);
+    setFollowCount(0);
+  };
 
   return (
     <div>
@@ -250,35 +286,25 @@ const Form: Component = () => {
           onInput={(e) => setPassword(e.currentTarget.value)}
         />
       </div>
-      <button
-        type="button"
-        onclick={() => fetchHiddenAccounts(handle(), password())}
-      >
-        Preview
-      </button>
-      <div>
-        <button type="button" onclick={() => selectRecords(RepoStatus.DELETED)}>
-          Deleted
-        </button>
+      <Show when={!followRecords.length}>
         <button
           type="button"
-          onclick={() => selectRecords(RepoStatus.DEACTIVATED)}
+          onclick={() => fetchHiddenAccounts(handle(), password())}
         >
-          Deactivated
+          Preview
         </button>
-        <button
-          type="button"
-          onclick={() => selectRecords(RepoStatus.BLOCKEDBY)}
-        >
-          Blocked By
+      </Show>
+      <Show when={followRecords.length}>
+        <button type="button" onclick={() => unfollow()}>
+          Confirm
         </button>
-        <button
-          type="button"
-          onclick={() => selectRecords(RepoStatus.SUSPENDED)}
-        >
-          Suspended
-        </button>
-      </div>
+      </Show>
+      <Show when={followCount()}>
+        <div>
+          Progress: {progress()}/{followCount()}
+        </div>
+        <br />
+      </Show>
     </div>
   );
 };
@@ -294,13 +320,7 @@ const App: Component = () => {
         <a href="https://github.com/notjuliet/cleanfollow-bsky">Source Code</a>
       </div>
       <Form />
-      <Show when={followCount()}>
-        <div>
-          Progress: {progress()}/{followCount()}
-        </div>
-        <br />
-      </Show>
-      <Records />
+      <Follows />
     </div>
   );
 };
