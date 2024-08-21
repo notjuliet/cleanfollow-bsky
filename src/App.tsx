f373a96bc93495ece62594b726c5e8385d8dd48e
notjuliet <notjuliet@riseup.net>
2024-08-21 11:49:29 +0200

    forgor https oops

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ let [loginState, setLoginState] = createSignal<boolean>();
 
 const client = await BrowserOAuthClient.load({
   clientId: "https://cleanfollow-bsky.pages.dev/client-metadata.json",
-  handleResolver: "boletus.us-west.host.bsky.network",
+  handleResolver: "http://boletus.us-west.host.bsky.network",
 });
 
 client.addEventListener("deleted", () => {
