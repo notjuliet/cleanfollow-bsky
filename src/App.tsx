17cde3a7125c26197a2cce7d487ca033f5b4ab2b
notjuliet <notjuliet@riseup.net>
2024-08-21 11:45:48 +0200

    fix handleresolver i think;

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ let [loginState, setLoginState] = createSignal<boolean>();
 
 const client = await BrowserOAuthClient.load({
   clientId: "https://cleanfollow-bsky.pages.dev/client-metadata.json",
-  handleResolver: "https://bsky.social",
+  handleResolver: "boletus.us-west.host.bsky.network",
 });
 
 client.addEventListener("deleted", () => {
