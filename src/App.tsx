c3bb149d6c4b202937dfc7a1336efeef539195f5
notjuliet <notjuliet@riseup.net>
2024-10-28 05:37:47 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -217,7 +217,7 @@ const Fetch: Component = () => {
 
     const follows = await fetchFollows();
     setFollowCount(follows.length);
-    let tmpFollows: FollowRecord[] = [];
+    const tmpFollows: FollowRecord[] = [];
 
     follows.forEach(async (record) => {
       let status: RepoStatus | undefined = undefined;
@@ -397,7 +397,7 @@ const Follows: Component = () => {
                       )
                     }
                   />
-                  <span class="peer relative h-5 w-9 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-4 after:w-4 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"></span>
+                  <span class="peer relative h-5 w-9 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-4 after:w-4 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800 rtl:peer-checked:after:-translate-x-full"></span>
                   <span class="ms-3 select-none dark:text-gray-300">
                     {option.label}
                   </span>
