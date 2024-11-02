f2b3d898d46541650d7aa50d07dffc3869893adb
notjuliet <notjuliet@riseup.net>
2024-11-02 07:43:38 +0100

    update bluesky link

--- src/App.tsx
+++ src/App.tsx
@@ -397,7 +397,7 @@ const Follows: Component = () => {
                       )
                     }
                   />
-                  <span class="peer relative h-5 w-9 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-4 after:w-4 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800 rtl:peer-checked:after:-translate-x-full"></span>
+                  <span class="peer relative h-5 w-9 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-4 after:w-4 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"></span>
                   <span class="ms-3 select-none dark:text-gray-300">
                     {option.label}
                   </span>
@@ -487,7 +487,7 @@ const App: Component = () => {
           <span> | </span>
           <a
             class="text-blue-600 hover:underline"
-            href="https://bsky.app/profile/bsky.mom"
+            href="https://bsky.app/profile/did:plc:b3pn34agqqchkaf75v7h43dk"
           >
             Bluesky
           </a>
