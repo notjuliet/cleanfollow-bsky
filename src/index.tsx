b02423d59a3340d82779e221bb3b20e45d571851
notjuliet <notjuliet@riseup.net>
2024-11-09 07:49:49 +0100

    remove index.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
 
-import "./index.css";
 import App from "./App";
 import "virtual:uno.css";
 import "@unocss/reset/tailwind-compat.css";
